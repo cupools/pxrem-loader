1f9bd5aaa7c3ee5e74b192c1315dbb968c5be04e
碎碎酱 <yinxin630@gmail.com>
2017-08-29 20:10:41 +0800

    feat: Update dependence. Supoort convert "transform". Fix webpack 2/3 warning

--- index.js
+++ index.js
@@ -2,9 +2,8 @@
 
 var pxrem = require('pxrem')
 var loaderUtils = require('loader-utils')
-var assign = require('lodash.assign')
 
 module.exports = function (content) {
-  var config = loaderUtils.getLoaderConfig(this, this.options ? 'pxrem' : '')
+  var config = loaderUtils.getOptions(this)
   return pxrem.process(content, config).toString()
 }
